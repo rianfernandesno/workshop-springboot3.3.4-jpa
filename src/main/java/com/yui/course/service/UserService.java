13373edadc2ff27963bd468b3de1419d66e52957
rianfernandesno <zrpapz12@gmail.com>
2024-10-08 23:05:06 -0300

    Excpetion handler

--- src/main/java/com/yui/course/service/UserService.java
+++ src/main/java/com/yui/course/service/UserService.java
@@ -2,6 +2,7 @@ package com.yui.course.service;
 
 import com.yui.course.entities.User;
 import com.yui.course.repositories.UserRepository;
+import com.yui.course.service.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,8 +21,9 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
+
 
     public User insert(User obj){
         return repository.save(obj);
