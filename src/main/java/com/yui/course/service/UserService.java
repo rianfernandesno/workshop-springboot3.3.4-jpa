189c4937755a6db2e077111ba9039ad573af82c4
rianfernandesno <zrpapz12@gmail.com>
2024-10-08 23:34:43 -0300

    update error

--- src/main/java/com/yui/course/service/UserService.java
+++ src/main/java/com/yui/course/service/UserService.java
@@ -4,6 +4,7 @@ import com.yui.course.entities.User;
 import com.yui.course.repositories.UserRepository;
 import com.yui.course.service.exceptions.DatabaseException;
 import com.yui.course.service.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -43,9 +44,15 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        UpdateData(entity, obj);
-        return repository.save(entity);
+
+        try{
+            User entity = repository.getReferenceById(id);
+            UpdateData(entity, obj);
+            return repository.save(entity);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
+
     }
 
     private void UpdateData(User entity, User obj) {
