8b5ba75d8456d91882cf6bb993e8a3baadbcc7ee
rianfernandesno <zrpapz12@gmail.com>
2024-10-08 23:31:05 -0300

    error delet

--- src/main/java/com/yui/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/yui/course/resources/exceptions/ResourceExceptionHandler.java
@@ -2,6 +2,7 @@ package com.yui.course.resources.exceptions;
 
 import com.yui.course.service.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,6 +17,14 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<StandardError> dataBase(DataAccessException e, HttpServletRequest request){
+        String error = "Data base ERROR";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
